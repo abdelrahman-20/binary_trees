b5c648d637a590f83563761d03d6e9af7b92643c
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-02-01 14:19:48 -0800

    Task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		leaves += (tree->left != NULL && tree->right != NULL) ? 1 : 0;
+		leaves += (tree->left == NULL && tree->right == NULL) ? 1 : 0;
 		leaves += binary_tree_leaves(tree->left);
 		leaves += binary_tree_leaves(tree->right);
 	}
